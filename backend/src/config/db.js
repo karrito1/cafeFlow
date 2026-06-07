1ea416043c00625526902471b0f5399221e971b5
karrito1 <kamilovilla554@gmail.com>
2026-06-07 15:38:56 -0500

    Configuracion MongoDB Atlas

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const conectarDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
+
     console.log('✅ MongoDB conectado');
   } catch (error) {
     console.error('❌ Error MongoDB:', error.message);
